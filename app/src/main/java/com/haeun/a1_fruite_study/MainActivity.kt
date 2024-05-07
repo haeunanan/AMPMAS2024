209db3846a4ac130a9220ee4037ed6fcd2ea9ae5
haeun <haeunanan@naver.com>
2024-05-07 18:10:30 +0900

    240507

--- app/src/main/java/com/haeun/a1_fruite_study/MainActivity.kt
+++ app/src/main/java/com/haeun/a1_fruite_study/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.haeun.a1_fruite_study
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -11,10 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        // 1. 사진을 클릭했을 때 메세지 띄우기 by Toast
         val image1 = findViewById<ImageView>(R.id.fruit_image_1)
         image1.setOnClickListener {
             Toast.makeText(this, "1번 사진 클릭", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면 다른 화면으로 넘어감
+            // 구글링 : "안드로이드 스튜디오 화면전환 kotlin"
+            var intent = Intent(this, Fruit1Activity::class.java)
+            startActivity(intent)
         }
 
     }
