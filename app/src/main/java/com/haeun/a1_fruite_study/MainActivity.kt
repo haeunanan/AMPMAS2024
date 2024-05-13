948dab287b84f63dc1e9e0eab0155c7941501a7e
haeun <haeunanan@naver.com>
2024-05-13 19:32:57 +0900

    24

--- app/src/main/java/com/haeun/a1_fruite_study/MainActivity.kt
+++ app/src/main/java/com/haeun/a1_fruite_study/MainActivity.kt
@@ -3,6 +3,7 @@ package com.haeun.a1_fruite_study
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
@@ -22,10 +23,50 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, Fruit1Activity::class.java)
             startActivity(intent)
         }
+    val btn1 = findViewById<ImageView>(R.id.fruit_image_1)
+    val btn2 = findViewById<ImageView>(R.id.fruit_image_2)
+    val btn3 = findViewById<ImageView>(R.id.fruit_image_3)
+    val btn4 = findViewById<ImageView>(R.id.fruit_image_4)
+    val btn5 = findViewById<ImageView>(R.id.fruit_image_5)
+    val btn6 = findViewById<ImageView>(R.id.fruit_image_6)
+    val btn7 = findViewById<ImageView>(R.id.fruit_image_7)
+
+        btn1.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data","1")
+            startActivity(intent)
+        }
+        btn2.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data","2")
+            startActivity(intent)
+        }
+        btn3.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data","3")
+            startActivity(intent)
+        }
+        btn4.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data","4")
+            startActivity(intent)
+        }
+        btn5.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data","5")
+            startActivity(intent)
+        }
+        btn6.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data","6")
+            startActivity(intent)
+        }
+        btn7.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data","7")
+            startActivity(intent)
+        }
+
 
     }
-
-
-
-
 }
